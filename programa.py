119e9983117ecbca6a2a9b85d932be81555d1c23
vanessaxmorais <141357535+vanessaxmorais@users.noreply.github.com>
2023-10-26 17:12:49 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,55 @@
+nome = "Vanessa"
+
+print(nome)
+
+def soma (x, y):
+    resultado =  x + y
+    return resultado
+
+def simples (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+def medio(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return 'tente outra cor'
+def completo(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    elif cor == 'marrom':
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
+def completo(cor):
+    if cor == 'azul':
+        return 'boa'
+    elif cor == 'roxo':
+        return 'adoro'
+    elif cor == 'preto':
+        return 'morreu'
+    elif cor == 'laranja':
+        return 'feião'
+    elif cor == 'amarelo':
+        return 'gostei'
+numeros = [1, 2, 3, 4, 5]
+print(numeros[0])
+print(numeros[-1])
+numeros[0] = 10
+print(numeros)
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+for i in range(10):
+    print(i)
+
+for item in [1,45,78,'a',[3,5]]:
+    print(item)
+
+for letra in 'minha string':
+    print(letra)
+
+
+    
